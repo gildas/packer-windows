7988b5f710e17760fc9d01bdaab2495433be2a57
Gildas Cherruel <gildas.cherruel@inin.com>
2014-12-11 03:17:10 +0900

    Winrm is stored in Specinfra

--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -1,13 +1,10 @@
 require 'serverspec'
 require 'winrm'
 
-include Serverspec::Helpers::Windows
-include Serverspec::Helpers::WinRM
+set :backend, :winrm
 
-RSpec.configure do |config|
-  config.before :suite do
-    endpoint = 'http://localhost:5985/wsman'
-    config.winrm = ::WinRM::WinRMWebService.new(endpoint, :plaintext, user: 'vagrant', pass: 'vagrant', disable_sspi: true)
-    config.winrm.set_timeout 300
-  end
-end
+endpoint = 'http://localhost:5985/wsman'
+winrm = ::WinRM::WinRMWebService.new(endpoint, user: 'vagrant', pass: 'vagrant', basic_auth_only: true)
+#winrm = ::WinRM::WinRMWebService.new(endpoint, :plaintext, user: 'vagrant', pass: 'vagrant', disable_sspi: true)
+winrm.set_timeout 300
+Specinfra.configuration.winrm = winrm
