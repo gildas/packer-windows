52fcae3ac8b9758c318212e5988b8e75f0696800
Gildas Cherruel <gildas.cherruel@inin.com>
2014-12-12 17:03:19 +0900

    Fixed firewall configuration

--- scripts/02-config-winrm.cmd
+++ scripts/02-config-winrm.cmd
@@ -9,7 +9,8 @@ cmd /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
 cmd /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 cmd /c winrm set winrm/config/service/auth @{Basic="true"}
 cmd /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
+cmd /c netsh firewall set service type=remoteadmin mode=enable
 cmd /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-cmd /c netsh firewall add portopening TCP 5985 "Port 5985"
+cmd /c netsh advfirewall firewall add rule name="Port 5985" dir=in action=allow protocol=TCP localport=5985
 cmd /c net stop winrm
 cmd /c net start winrm
