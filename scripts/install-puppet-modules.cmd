9a53173dd6590381bad2bd9925936d13c52fb9ab
Gildas Cherruel <gildas.cherruel@inin.com>
2014-12-16 23:20:07 +0900

    Added Puppet modules to preload

---
+++ scripts/install-puppet-modules.cmd
@@ -0,0 +1,5 @@
+cmd /c certutil -addstore  "Root" C:\Windows\Temp\GeoTrust_Global_CA.pem
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-stdlib
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-registry
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-powershell
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install gildas-sqlserver
