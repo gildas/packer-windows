094903dfe8099e790148a20fbdf61703091ec5e9
Gildas Cherruel <gildas.cherruel@inin.com>
2014-12-17 08:16:45 +0900

    More puppet modules for Windows

--- scripts/install-puppet-modules.cmd
+++ scripts/install-puppet-modules.cmd
@@ -1,6 +1,10 @@
 cmd /c certutil -addstore  "Root" C:\Windows\Temp\GeoTrust_Global_CA.pem
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-stdlib
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-registry
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-dism
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-acl
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-reboot
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-inifile
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-powershell
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install gildas-sqlserver
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install PierrickL-cicserver
