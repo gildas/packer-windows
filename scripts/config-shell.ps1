ad13dd974b3656798ce4195c3d017a06622b3fae
Gildas Cherruel <gildas.cherruel@inin.com>
2014-12-16 15:37:42 +0900

    Make Powershell the default shell when logging in

--- scripts/config-shell.ps1
+++ scripts/config-shell.ps1
@@ -1,1 +1,1 @@
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name Shell -Value 'Powershell.exe -NoExit -Command "$PSVersionTable; cd $env:userprofile"'
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name Shell -Value 'Powershell.exe -NoExit -Command "$PSVersionTable; cd $env:userprofile"' -Force
