fe0159fee0f89ff4122c9ed45ca024a3d42b88cd
Gildas Cherruel <gildas.cherruel@inin.com>
2015-01-06 11:47:06 +0900

    Use Powershell to install guest addition on Windows 2012R2 full

--- scripts/install-vmhost-additions.ps1
+++ scripts/install-vmhost-additions.ps1
@@ -1,10 +1,86 @@
-$disk  = Mount-DiskImage C:\Users\vagrant\VBoxGuestAdditions.iso
-if ($? -ne 0) { Write-Error "ERROR %ERRORLEVEL% while adding Oracle certificate to the trusted publishers" }
-$drive = $disk.DriveLetter
+<#
+#>
 
-certutil -addstore -f "TrustedPublisher" ${drive}\cert\oracle-vbox.cer
-if ($? -ne 0) { Write-Error "ERROR %ERRORLEVEL% while adding Oracle certificate to the trusted publishers" }
-
-${drive}:\VBoxWindowsAdditions.exe /S
-if { $? -ne 0) { Write-Error "ERROR %ERRORLEVEL% while installing Virtualbox Guest Additions" }
-Dismount-DiskImage $disk.ImagePath
+if ($env:PACKER_BUILDER_TYPE -match 'vmware')
+{
+  $iso_path="C:\Users\vagrant\windows.iso"
+  if (Test-Path $iso_path)
+  {
+    Write-Host "Mounting ISO $iso_path"
+    $image = Mount-DiskImage $iso_path -PassThru
+    if (! $?)
+    {
+      Write-Error "ERROR $LastExitCode while mounting VMWare Guest Additions"
+      Start-Sleep 10
+      exit 2
+    }
+    $drive = (Get-Volume -DiskImage $image).DriveLetter
+    Write-Host "ISO Mounted on $drive"
+    Write-Host "Installing VMWare Guest Additions"
+    Start-Process ${drive}:\setup.exe -ArgumentList '/S','/v','"/qb! /l*v C:\Windows\Logs\vmware-tools.log REBOOT=R ADDLOCAL=ALL"' -Wait
+    if (! $?)
+    {
+      Write-Error "ERROR $LastExitCode while installing VMWare Guest Additions"
+      Start-Sleep 10
+      exit 2
+    }
+    Write-Host "Dismounting ISO"
+    Dismount-DiskImage $image.ImagePath
+    Write-Host "Restarting Virtual Machine"
+    Restart-Computer
+    Start-Sleep 30
+  }
+  else
+  {
+    Write-Error "ISO Not found in [$iso_path]"
+    exit 2
+  }
+}
+elseif ($env:PACKER_BUILDER_TYPE -match 'virtualbox')
+{
+  $iso_path="C:\Users\vagrant\VBoxGuestAdditions.iso"
+  if (Test-Path $iso_path)
+  {
+    Write-Host "Mounting ISO $iso_path"
+    $image = Mount-DiskImage $iso_path
+    if (! $?)
+    {
+      Write-Error "ERROR $LastExitCode while mounting VMWare Guest Additions"
+      Start-Sleep 10
+      exit 2
+    }
+    $drive = (Get-Volume -DiskImage $image).DriveLetter
+    Write-Host "ISO Mounted on $drive"
+    # cd ${drive}:\cert ; VBoxCertUtil add-trusted-publisher oracle-vbox.cer --root oracle-vbox.cer
+    certutil -addstore -f "TrustedPublisher" ${drive}:\cert\oracle-vbox.cer
+    if (! $?)
+    {
+      Write-Error "ERROR $LastExitCode while adding Oracle certificate to the trusted publishers"
+      Start-Sleep 10
+      exit 2
+    }
+    Write-Host "Installing Virtualbox Guest Additions"
+    Start-Process ${drive}:\VBoxWindowsAdditions.exe -ArgumentList '/S' -Wait
+    if (! $?)
+    {
+      Write-Error "ERROR $LastExitCode while installing Virtualbox Guest Additions"
+      Start-Sleep 10
+      exit 2
+    }
+    Write-Host "Dismounting ISO"
+    Dismount-DiskImage $image.ImagePath
+    Write-Host "Restarting Virtual Machine"
+    Restart-Computer
+    Start-Sleep 30
+  }
+  else
+  {
+    Write-Error "ISO Not found in [$iso_path]"
+    exit 2
+  }
+}
+else
+{
+  Write-Error "Unsupported Packer builder: $env:PACKER_BUILDER_TYPE"
+  exit 1
+}
