96ade2894bca01e0989380a7cad886c2086bb244
Gildas Cherruel <gildas.cherruel@inin.com>
2015-01-07 07:29:49 +0900

    Discover CIC Installation MSI automatically

--- scripts/install-cic.ps1
+++ scripts/install-cic.ps1
@@ -4,12 +4,13 @@
 #> # }}}
 [CmdletBinding()] 
 Param(
-  [Parameter(Mandatory=$false)][string] $User            = 'vagrant',
-  [Parameter(Mandatory=$false)][string] $Password        = 'vagrant',
-  [Parameter(Mandatory=$false)][string] $InstallDiskPath = 'E:\',
-  [Parameter(Mandatory=$false)][string] $InstallPath     = 'C:\I3\IC',
+  [Parameter(Mandatory=$false)][string] $User          = 'vagrant',
+  [Parameter(Mandatory=$false)][string] $Password      = 'vagrant',
+  [Parameter(Mandatory=$false)][string] $InstallPath   = 'C:\I3\IC',
+  [Parameter(Mandatory=$false)][string] $InstallSource,
   [Parameter(Mandatory=$false)][switch] $Reboot
 )
+$Now = Get-Date -Format 'yyyyMMddHHmmss'
 
 # Prerequisites: {{{
 # Prerequisite: Powershell 3 {{{2
@@ -21,8 +22,33 @@ if($PSVersionTable.PSVersion.Major -lt 3)
 }
 # 2}}}
 
+# Prerequisite: Find the source! {{{2
+if (!$InstallSource)
+{
+  $sources  = @( 'C:\Windows\Temp' )
+
+  $sources += (Get-Volume | Where { $_.DriveType -eq 'CD-ROM' -and $_.Size -gt 0 } | ForEach { $_.DriveLetter + ':\Installs\ServerComponents' })
+
+  ForEach ($source in $sources)
+  {
+    Write-Debug "  Searching in $source"
+    if (Test-Path "${source}\ICServer_2015_R1.msi")
+    {
+      $InstallSource = $source
+      break
+    }
+  }
+  if (!$InstallSource)
+  {
+    Write-Error "CIC Installation source not found, please provide a source via the command line arguments"
+    return 1
+  }
+}
+Write-Verbose "Installing CIC from $InstallSource"
+# 2}}}
+
 # Prerequisite: .Net 3.5 {{{2
-if ((Get-WindowsFeature Net-Framework-Core).InstallState -ne 'Installed')
+if ((Get-WindowsFeature Net-Framework-Core -Verbose:$false).InstallState -ne 'Installed')
 {
   Write-Verbose "Installing .Net 3.5"
   Install-WindowsFeature -Name Net-Framework-Core
@@ -37,10 +63,10 @@ if (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*
 {
   Write-Verbose "$Product is already installed"
 }
-elseif (! (Test-Path 'E:\Installs\ServerComponents\ICServer_2015_R1.msi'))
+elseif (! (Test-Path "${InstallSource}\ICServer_2015_R1.msi"))
 {
   #TODO: Should we return values or raise exceptions?
-  Write-Error "Cannot install $Product, MSI not found"
+  Write-Error "Cannot install $Product, MSI not found in $InstallSource"
   return -1
 }
 else
@@ -49,7 +75,7 @@ else
   #TODO: Capture the domain if it is in $User
   $Domain = $env:COMPUTERNAME
 
-  $parms  = '/i','E:\Installs\ServerComponents\ICServer_2015_R1.msi'
+  $parms  = '/i',"${InstallSource}\ICServer_2015_R1.msi"
   $parms += "PROMPTEDUSER=$User"
   $parms += "PROMPTEDDOMAIN=$Domain"
   $parms += "PROMPTEDPASSWORD=$Password"
@@ -60,7 +86,7 @@ else
   $parms += 'OVERRIDEKBREQUIREMENT=1'
   $parms += 'REBOOT=ReallySuppress'
   $parms += '/l*v'
-  $parms += 'C:\Windows\Logs\icserver.log'
+  $parms += "C:\Windows\Logs\icserver-${Now}.log"
   $parms += '/qb!'
   $parms += '/norestart'
 
@@ -74,21 +100,21 @@ if (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion
 {
   Write-Verbose "$Product is already installed"
 }
-elseif (! (Test-Path 'E:\Installs\ServerComponents\InteractionFirmware_2015_R1.msi'))
+elseif (! (Test-Path "${InstallSource}\InteractionFirmware_2015_R1.msi"))
 {
   #TODO: Should we return values or raise exceptions?
-  Write-Error "Cannot install $Product, MSI not found"
+  Write-Error "Cannot install $Product, MSI not found in $InstallSource"
   return -1
 }
 else
 {
   Write-Verbose "Installing $Product"
 
-  $parms  = '/i','E:\Installs\ServerComponents\InteractionFirmware_2015_R1.msi'
+  $parms  = '/i',"${InstallSource}\InteractionFirmware_2015_R1.msi"
   $parms += 'STARTEDBYEXEORIUPDATE=1'
   $parms += 'REBOOT=ReallySuppress'
   $parms += '/l*v'
-  $parms += 'C:\Windows\Logs\icfirmware.log'
+  $parms += "C:\Windows\Logs\icfirmware-${Now}.log"
   $parms += '/qb!'
   $parms += '/norestart'
 
