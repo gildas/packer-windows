2648e6f7f92be6f2ffd871a04bf2aa4c62a92f7f
Gildas Cherruel <gildas.cherruel@inin.com>
2015-01-30 11:43:14 +0900

    Added support for Parallels

--- scripts/install-vmhost-additions.ps1
+++ scripts/install-vmhost-additions.ps1
@@ -75,6 +75,42 @@ elseif ($env:PACKER_BUILDER_TYPE -match 'virtualbox')
     Write-Host "ISO was not loaded [$iso_path], nothing will happen"
   }
 }
+elseif ($env:PACKER_BUILDER_TYPE -match 'parallels')
+{
+  $iso_path="C:\Users\vagrant\prl-tools-win.iso"
+  if (Test-Path $iso_path)
+  {
+    Write-Host "Mounting ISO $iso_path"
+    $image = Mount-DiskImage $iso_path -PassThru
+    if (! $?)
+    {
+      Write-Error "ERROR $LastExitCode while mounting VMWare Guest Additions"
+      Start-Sleep 10
+      exit 2
+    }
+    $drive = (Get-Volume -DiskImage $image).DriveLetter
+    Write-Host "ISO Mounted on $drive"
+    # cd ${drive}:\cert ; VBoxCertUtil add-trusted-publisher oracle-vbox.cer --root oracle-vbox.cer
+    Write-Host "Installing Parallels Guest Additions"
+    Start-Process ${drive}:\PTAgent.exe -ArgumentList '/install_silent' -Wait
+    if (! $?)
+    {
+      Write-Error "ERROR $LastExitCode while installing Parallels Guest Additions"
+      Start-Sleep 10
+      exit 2
+    }
+    Start-Sleep 20
+    Write-Host "Dismounting ISO"
+    Dismount-DiskImage -ImagePath $image.ImagePath
+#    Write-Host "Restarting Virtual Machine"
+#    Restart-Computer
+#    Start-Sleep 30
+  }
+  else
+  {
+    Write-Host "ISO was not loaded [$iso_path], nothing will happen"
+  }
+}
 else
 {
   Write-Error "Unsupported Packer builder: $env:PACKER_BUILDER_TYPE"
