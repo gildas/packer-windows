6306811fcc7795a6abba9117d7d8963920b37723
Gildas Cherruel <gildas.cherruel@inin.com>
2015-04-23 03:26:22 +0900

    Virtualbox addition is now via attached ISO

--- scripts/install-vmhost-additions.ps1
+++ scripts/install-vmhost-additions.ps1
@@ -31,45 +31,34 @@ if ($env:PACKER_BUILDER_TYPE -match 'vmware')
 }
 elseif ($env:PACKER_BUILDER_TYPE -match 'virtualbox')
 {
-  $iso_path="C:\Users\vagrant\VBoxGuestAdditions.iso"
-  if (Test-Path $iso_path)
+  $drive='E'
+  # cd ${drive}:\cert ; VBoxCertUtil add-trusted-publisher oracle-vbox.cer --root oracle-vbox.cer
+  certutil -addstore -f "TrustedPublisher" ${drive}:\cert\oracle-vbox.cer
+  if (! $?)
   {
-    Write-Host "Mounting ISO $iso_path"
-    $image = Mount-DiskImage $iso_path -PassThru
-    if (! $?)
-    {
-      Write-Error "ERROR $LastExitCode while mounting VMWare Guest Additions"
-      Start-Sleep 10
-      exit 2
-    }
-    $drive = (Get-Volume -DiskImage $image).DriveLetter
-    Write-Host "ISO Mounted on $drive"
-    # cd ${drive}:\cert ; VBoxCertUtil add-trusted-publisher oracle-vbox.cer --root oracle-vbox.cer
-    certutil -addstore -f "TrustedPublisher" ${drive}:\cert\oracle-vbox.cer
-    if (! $?)
-    {
-      Write-Error "ERROR $LastExitCode while adding Oracle certificate to the trusted publishers"
-      Start-Sleep 10
-      exit 2
-    }
-    Write-Host "Installing Virtualbox Guest Additions"
-    Start-Process ${drive}:\VBoxWindowsAdditions.exe -ArgumentList '/S' -Wait
-    if (! $?)
-    {
-      Write-Error "ERROR $LastExitCode while installing Virtualbox Guest Additions"
-      Start-Sleep 10
-      exit 2
-    }
-    Write-Host "Dismounting ISO"
-    Dismount-DiskImage -ImagePath $image.ImagePath
+    Write-Error "ERROR $LastExitCode while adding Oracle certificate to the trusted publishers"
+    Start-Sleep 10
+    exit 2
+  }
+  Write-Host "Installing Virtualbox Guest Additions"
+  $process = Start-Process -Wait -PassThru -FilePath ${drive}:\VBoxWindowsAdditions.exe -ArgumentList '/S /l C:\Windows\Logs\vmware-tools.log /v"/qn REBOOT=R"'
+  if ($process.ExitCode -eq 0)
+  {
+    Write-Host "Installation was successful"
+  }
+  elseif ($process.ExitCode -eq 3010)
+  {
+    Write-Warning "Installation was successful, Rebooting is needed"
 #    Write-Host "Restarting Virtual Machine"
 #    Restart-Computer
 #    Start-Sleep 30
   }
   else
   {
-    Write-Host "ISO was not loaded [$iso_path], nothing will happen"
+    Write-Error "Installation failed: Error= $($process.ExitCode), Logs=C:\Windows\Logs\vmware-tools.log"
+    Start-Sleep 2; exit $process.ExitCode
   }
+  Start-Sleep 2
 }
 elseif ($env:PACKER_BUILDER_TYPE -match 'parallels')
 {
