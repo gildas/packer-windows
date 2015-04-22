ef4ff7e3531ead893bb7d5e288e5d89e7b3eece1
Gildas Cherruel <gildas.cherruel@inin.com>
2015-04-23 08:47:39 +0900

    beefed up winrm config

--- scripts/02-config-winrm.cmd
+++ scripts/02-config-winrm.cmd
@@ -2,15 +2,17 @@
 setlocal EnableDelayedExpansion EnableExtensions
 title Configuring Windows Remote Management...
 
+cmd /c net stop winrm
 cmd /c winrm quickconfig -q
 cmd /c winrm quickconfig -transport:http
 cmd /c winrm set winrm/config @{MaxTimeoutms="1800000"}
-cmd /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+cmd /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="2048"}
 cmd /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 cmd /c winrm set winrm/config/service/auth @{Basic="true"}
 cmd /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
+cmd /c sc config winrm start= auto
+cmd /c net start winrm
+
 cmd /c netsh firewall set service type=remoteadmin mode=enable
 cmd /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
 cmd /c netsh advfirewall firewall add rule name="Port 5985" dir=in action=allow protocol=TCP localport=5985
-cmd /c net stop winrm
-cmd /c net start winrm
