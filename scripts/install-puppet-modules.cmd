aff1accbcd09a87cd6b7c61ac417a5d6b643e810
Gildas Cherruel <gildas.cherruel@inin.com>
2015-04-23 03:16:39 +0900

    puppet module installer deletes its certificate file

--- scripts/install-puppet-modules.cmd
+++ scripts/install-puppet-modules.cmd
@@ -1,4 +1,5 @@
 cmd /c certutil -addstore  "Root" C:\Windows\Temp\GeoTrust_Global_CA.pem
+cmd /c del C:\Windows\Temp\GeoTrust_Global_CA.pem
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-stdlib
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-registry
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-dism
