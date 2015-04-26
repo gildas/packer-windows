dbb55b4d70aa7b7fb5e44d7f5e6039827db31b9d
Gildas Cherruel <gildas.cherruel@inin.com>
2015-04-27 00:50:13 +0900

    Removed old code

--- scripts/install-icserver.ps1
+++ scripts/install-icserver.ps1
@@ -7,7 +7,7 @@ Param(
   [Parameter(Mandatory=$false)][string] $User          = 'vagrant',
   [Parameter(Mandatory=$false)][string] $Password      = 'vagrant',
   [Parameter(Mandatory=$false)][string] $InstallPath   = 'C:\I3\IC',
-  [Parameter(Mandatory=$false)][string] $InstallSource,
+  [Parameter(Mandatory=$true)][string]  $SourceDriveLetter = 'Z',
   [Parameter(Mandatory=$false)][switch] $Reboot
 )
 Write-Output "Script started at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
@@ -28,100 +28,13 @@ if($PSVersionTable.PSVersion.Major -lt 3)
 # 2}}}
 
 # Prerequisite: Find the source! {{{2
-if (!$InstallSource)
+$InstallSource = ${SourceDriveLetter} + ':\Installs\ServerComponents'
+if (! (Test-Path (Join-Path $InstallSource $Source_filename)))
 {
-  $sources  = @( 'C:\Windows\Temp' )
-  $sources += (Get-Volume | Where { $_.DriveType -eq 'CD-ROM' -and $_.Size -gt 0 } | ForEach { $_.DriveLetter + ':\Installs\ServerComponents' })
-
-  ForEach ($source in $sources)
-  {
-    Write-Debug "  Searching in $source"
-    if (Test-Path "${source}\${Source_filename}")
-    {
-      Write-Output "Found install in $source, validating checksum"
-      if ($(C:\tools\sysinternals\Get-Checksum.ps1 -MD5 -Path ${source}\${Source_filename} -eq $Source_checksum))
-      {
-        Write-Output "Found a valid install in $source"
-        $InstallSource = $source
-        break
-      }
-      Write-Debug "Found an invalid source in $source"
-    }
-  }
-  if (!$InstallSource)
-  {
-    Write-Error "IC Server Installation source not found, please provide a source via the command line arguments"
-    exit 1
-  }
-}
-elseif ($InstallSource -match 'http://.*')
-{
-  $source = 'C:\Windows\Temp'
-  if ((Test-Path "${source}\${Source_filename}") -and ($(C:\tools\sysinternals\Get-Checksum.ps1 -MD5 -Path ${source}\${Source_filename} -eq $Source_checksum)))
-  {
-    Write-Output "Installation has been downloaded already and is valid"
-    $InstallSource = 'C:\Windows\Temp'
-  }
-  else
-  {
-    if ($InstallSource -match 'http://\*:([0-9]+)')
-    {
-      $port    = $matches[1]
-      $address = ((Get-NetIPConfiguration).IPv4DefaultGateway).NextHop
-      $InstallSource = "http://${address}:${port}"
-    }
-    for ($i=0; $i -lt $Source_download_tries; $i++)
-    {
-      Write-Output "Downloading from $InstallSource, try: #${i}/${Source_download_tries}"
-      Try
-      {
-        (New-Object System.Net.WebClient).DownloadFile("${InstallSource}/${Source_filename}", "${source}\${Source_filename}")
-        if (Test-Path "${source}\${Source_filename}")
-        {
-          Write-Output "Downloaded install in $source, validating checksum"
-          if ($(C:\tools\sysinternals\Get-Checksum.ps1 -MD5 -Path ${source}\${Source_filename} -eq $Source_checksum))
-          {
-            Write-Output "Downloaded a valid install in $source"
-            $InstallSource = $source
-            break
-          }
-          Write-Warning "Downloaded source is corrupted, trying again"
-        }
-      }
-      Catch
-      {
-        Write-Warning "Cannot download source from $source, trying again"
-      }
-    }
-    if ($i -lt $Source_download_tries)
-    {
-      $InstallSource = 'C:\Windows\Temp'
-    }
-    else
-    {
-      Write-Error "Installation was not downloaded properly"
-      exit 1
-    }
-  }
-}
-elseif (Test-Path "${InstallSource}\${Source_filename}")
-{
-  Write-Output "Found install in ${InstallSource}, validating checksum"
-  if ($(C:\tools\sysinternals\Get-Checksum.ps1 -MD5 -Path ${InstallSource}\${Source_filename} -eq $Source_checksum))
-  {
-    Write-Output "Found a valid install in ${InstallSource}"
-  }
-  else
-  {
-    Write-Error "Found an invalid source in ${InstallSource}"
-    exit 1
-  }
-}
-else
-{
-  Write-Error "Source not found in $source"
+  Write-Error "IC Server Installation source not found in ${SourceDriveLetter}:"
   exit 1
 }
+
 Write-Output "Installing CIC from $InstallSource"
 # 2}}}
 
