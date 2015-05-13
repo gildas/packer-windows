0989059a9fb4d7661c0104d60fc8c3b74b210894
Gildas Cherruel <gildas.cherruel@inin.com>
2015-05-14 01:33:27 +0900

    Installing dotnet 3.5 separately

---
+++ scripts/install-dotnet35.ps1
@@ -0,0 +1,36 @@
+# .Net 3.5 Installation
+#
+#
+
+<# # Documentation {{{
+  .Synopsis
+  Installs .Net 3.5
+#> # }}}
+[CmdletBinding()] 
+Param(
+)
+Write-Output "Script started at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+$Now = Get-Date -Format 'yyyyMMddHHmmss'
+
+# Prerequisites: {{{
+# Prerequisite: Powershell 3 {{{2
+if($PSVersionTable.PSVersion.Major -lt 3)
+{
+    Write-Error "Powershell version 3 or more recent is required"
+    #TODO: Should we return values or raise exceptions?
+    exit 1
+}
+# 2}}}
+
+# Prerequisite: .Net 3.5 {{{2
+if ((Get-WindowsFeature Net-Framework-Core -Verbose:$false).InstallState -ne 'Installed')
+{
+  Write-Output "Installing .Net 3.5"
+  Install-WindowsFeature -Name Net-Framework-Core
+  # TODO: Check for errors
+}
+# 2}}}
+# Prerequisites }}}
+
+Start-Sleep 5
+Write-Output "Script ended at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
