3c5072ed733342240e122df8272b8794da79a007
Gildas Cherruel <gildas.cherruel@inin.com>
2015-05-18 19:06:15 +0900

    Adding CIC logs if any

--- scripts/Backup-Logs.ps1
+++ scripts/Backup-Logs.ps1
@@ -24,4 +24,14 @@ $log_dir = Join-Path $share_name "log"
 
 Copy-Item C:\ProgramData\chocolatey\logs\chocolatey.log (Join-Path $log_dir "packer-build-${env:PACKER_BUILDER_TYPE}-${env:PACKER_BUILD_NAME}-chocolatey.log")
 
+if (Test-Path "C:\Windows\Logs\icserver-*.log")
+{
+  Copy-Item C:\Windows\Logs\icserver-*.log (Join-Path $log_dir "packer-build-${env:PACKER_BUILDER_TYPE}-${env:PACKER_BUILD_NAME}-icserver.log")
+}
+
+if (Test-Path "C:\Windows\Logs\icfirmware-*.log")
+{
+  Copy-Item C:\Windows\Logs\icfirmware-*.log (Join-Path $log_dir "packer-build-${env:PACKER_BUILDER_TYPE}-${env:PACKER_BUILD_NAME}-icfirmware.log")
+}
+
 Start-Sleep 5
