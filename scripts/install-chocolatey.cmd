6c6e37e1ec0de4397208567894223b4306f3af4c
Gildas Cherruel <gildas.cherruel@inin.com>
2015-09-23 23:25:47 +0900

    Removed unused scripts

--- scripts/install-chocolatey.cmd
+++
@@ -1,5 +0,0 @@
-@if not defined PACKER_DEBUG (@echo off) else (@echo on)
-setlocal EnableDelayedExpansion EnableExtensions
-title Installing Chocolatey...
-
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
