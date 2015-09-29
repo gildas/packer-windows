50ac9e4fad5d6e80bce0eaf6c925b928547524ec
Gildas Cherruel <gildas.cherruel@inin.com>
2015-09-29 22:47:37 +0900

    More resilient chocolatey install

--- scripts/install-chocolatey.ps1
+++ scripts/install-chocolatey.ps1
@@ -1,1 +1,11 @@
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+(new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1', 'C:\Windows\Temp\install.ps1')
+
+for($try = 0; $try -lt 5; $try++)
+{
+  & C:/Windows/Temp/install.ps1
+  if ($?) { exit 0 }
+}
+Write-Error "Chocolatey failed to install, please re-build your machine again"
+exit 2
