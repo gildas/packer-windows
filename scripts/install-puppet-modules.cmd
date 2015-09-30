6ccef94ec9323d47c72febd430644a3b88a1d13e
Gildas Cherruel <gildas.cherruel@inin.com>
2015-09-30 10:05:01 +0900

    Use the new puppetlabs-windows module

--- scripts/install-puppet-modules.cmd
+++ scripts/install-puppet-modules.cmd
@@ -1,11 +1,6 @@
 cmd /c certutil -addstore  "Root" C:\Windows\Temp\GeoTrust_Global_CA.pem
 cmd /c del C:\Windows\Temp\GeoTrust_Global_CA.pem
-cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-stdlib
-cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-registry
+cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-windows
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-dism
-cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-acl
-cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-reboot
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-inifile
-cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install puppetlabs-powershell
-cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install chocolatey-chocolatey
 cmd /c "C:\Program Files\Puppet Labs\Puppet\bin\puppet.bat" module install gildas-firewall
