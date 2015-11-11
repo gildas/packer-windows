1ea83c550382d21f67382047a201ee5a5817a9ad
Gildas Cherruel <gildas.cherruel@inin.com>
2015-11-11 14:55:37 +0900

    Backup logs for Hyper-V

--- scripts/Backup-Logs.ps1
+++ scripts/Backup-Logs.ps1
@@ -4,15 +4,26 @@
 
 if ($env:PACKER_BUILDER_TYPE -match 'vmware')
 {
-  $share_name = "\\vmware-host\Shared Folders"
+  $log_dir = "\\vmware-host\Shared Folders\log"
 }
 elseif ($env:PACKER_BUILDER_TYPE -match 'virtualbox')
 {
-  $share_name = "\\vboxsrv"
+  $log_dir = "\\vboxsrv\log"
 }
 elseif ($env:PACKER_BUILDER_TYPE -match 'parallels')
 {
-  $share_name = "\\psf"
+  $log_dir = Join-Path $share_name "\\psf\log"
+}
+elseif ($env:PACKER_BUILDER_TYPE -match 'hyperv-iso')
+{
+  if ([string]::IsNullOrEmpty($env:SMBHOST))  { Write-Error "Environment variable SMBHOST is empty"  ; exit 1 }
+  if ([string]::IsNullOrEmpty($env:SMBSHARE)) { Write-Error "Environment variable SMBSHARE is empty" ; exit 1 }
+  if ([string]::IsNullOrEmpty($env:SMBUSER))  { Write-Error "Environment variable SMBUSER is empty"  ; exit 1 }
+  if ([string]::IsNullOrEmpty($env:SMBPASS))  { Write-Error "Environment variable SMBPASS is empty"  ; exit 1 }
+  $DriveLetter = ls function:[d-z]: -n | ?{ !(Test-Path $_) } | Select -Last 1
+  Write-Output "Mounting $($env:SMBSHARE) from $($env:SMBHOST) on $DriveLetter as $($env:SMBUSER)"
+  $Drive = New-PSDrive -Name $DriveLetter.Substring(0,1) -PSProvider FileSystem -Root \\${env:SMBHOST}\${env:SMBSHARE} -Credential (New-Object System.Management.Automation.PSCredential("${env:SMBHOST}\${env:SMBUSER}", ($env:SMBPASS | ConvertTo-SecureString -AsPlainText -Force)))
+  $log_dir = $DriveLetter
 }
 else
 {
